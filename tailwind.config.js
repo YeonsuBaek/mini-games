a11febdeebe14e24dace4ee466d927558069d0ca
yeonsuBaek <soothbugger@gmail.com>
2023-01-18 16:08:35 +0900

    (#2) feat: 폰트 스타일 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Alfa Slab One", "cursive"],
+        content: ["Arial"],
+      },
+    },
   },
   plugins: [],
 };
